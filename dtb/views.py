ee4d984e0042094324893fa0f2a97534077432f7
sergeymi37 <sergey.mikhaylenko@gmail..com>
2023-09-04 16:55:48 +0300

    fix iris

--- dtb/views.py
+++ dtb/views.py
@@ -25,16 +25,17 @@ def index(request):
     return JsonResponse({"ok": "wellcome"})
 
 def iris_info(request):
-    return JsonResponse({"iris_zts": str(classMethod("apptools.core.telebot", "TS", "Info"))})
+    return JsonResponse({"iris_zts": str(classMethod("apptools.core.telebot", "TS", 
+    f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}"))})
 
 def iris_zts(request):
-    return {"iris_zts": str(classMethod("apptools.core.telebot", "TS", "Info"))}
+    return JsonResponse({"iris_zts": str(classMethod("apptools.core.telebot", "TS", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}"))})
 
 def iris_ss(request):
-    return JsonResponse({"iris_ss": str(classMethod("apptools.core.telebot", "SS", "Info"))})
+    return JsonResponse({"iris_ss": str(classMethod("apptools.core.telebot", "SS", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}"))})
 
 def iris_alerts(request):
-    return JsonResponse({"iris_alerts": str(classMethod("apptools.core.telebot", "Alerts", "Info"))})
+    return JsonResponse({"iris_alerts": str(classMethod("apptools.core.telebot", "Alerts", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}"))})
 
 class TelegramBotWebhookView(View):
     # WARNING: if fail - Telegram webhook will be delivered again.
@@ -81,7 +82,7 @@ def index_page(request):
     context = {
         'pagename': _('Param Demo'),
         "errors": errors,
-        "iris_footer":classMethod("apptools.core.telebot", "GetFooter", "Info"),
+        "iris_footer":json.loads(classMethod("apptools.core.telebot", "GetFooter", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}")),
     }
 
     return render(request, 'pages/index.html', context)
@@ -100,7 +101,7 @@ def add_param_page(request):
         form = ParamForm()
         context = {
             'pagename': _('Adding a new parameter'),
-            "iris_footer":classMethod("apptools.core.telebot", "GetFooter", "Info"),
+            "iris_footer":json.loads(classMethod("apptools.core.telebot", "GetFooter", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}")),
             'form': form
         }
         return render(request, 'pages/add_param.html', context)
@@ -111,8 +112,8 @@ def param_detail(request, param_id):
     comment_form = CommentForm()
     comments = param.comments.all()
     context = {
-        'pagename': _('Options Page'),
-        "iris_footer":classMethod("apptools.core.telebot", "GetFooter", "Info"),
+       'pagename': _('Options Page'),
+       "iris_footer":json.loads(classMethod("apptools.core.telebot", "GetFooter", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}")),
         "param": param,
         "comments": comments,
         "comment_form": comment_form,
@@ -142,7 +143,7 @@ def params_page(request,my=False):
         params = params.filter(name__icontains=param_context) | params.filter(code__icontains=param_context)
     count=params.count()
     context = {
-        "iris_footer":classMethod("apptools.core.telebot", "GetFooter", "Info"),
+        "iris_footer":json.loads(classMethod("apptools.core.telebot", "GetFooter", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}")),
         'pagename': pagename,
         'params': params,
         'count':count
@@ -155,7 +156,7 @@ def params_my(request):
     params = params = Param.objects.filter(user=request.user)
     context = {
         'pagename': _('My parameters'),
-        "iris_footer":classMethod("apptools.core.telebot", "GetFooter", "Info"),
+         "iris_footer":json.loads(classMethod("apptools.core.telebot", "GetFooter", f"{request.user},{request.user.is_authenticated},{request.user.is_authenticated}")),
         'params': params
     }
     return render(request, 'pages/view_params.html', context)
